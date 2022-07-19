5858b229e825e9519f7c74d5b1e7f7c2de788ed2
Dolendra Sharma <dolensharma12@gmail.com>
2022-07-19 09:28:28 +0545

    demo

---
+++ demo/src/test/java/com/therisingavocado/employeemanagement/EmpSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.therisingavocado.employeemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
